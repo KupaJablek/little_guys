2341134fe5c7f59cec3920ea633a76077fa05ed6
YertleT <Ian.g128@hotmail.ca>
2025-04-08 16:19:13 -0400

    Added player interaction
    Players can now input directional keys. A properly timed directional key will properly cause arrows to disappear.

--- levels/heart/heart.gd
+++ levels/heart/heart.gd
@@ -3,14 +3,19 @@ class_name Heart
 
 @onready var arrow_spawns : Array[Node] = $Sprite2D/ScreenOverlay/ArrowSpawns.get_children()
 @onready var arrow_despawn_point : Node2D = $ArrowDespawnPoint
+@onready var left_hitbox : Area2D = $Sprite2D/ScreenOverlay/ScreenArrows/LArrow
+@onready var up_hitbox : Area2D = $Sprite2D/ScreenOverlay/ScreenArrows/UArrow
+@onready var down_hitbox : Area2D = $Sprite2D/ScreenOverlay/ScreenArrows/DArrow
+@onready var right_hitbox : Area2D = $Sprite2D/ScreenOverlay/ScreenArrows/RArrow
 
 @export var arrow_speed : float = 100.0
 
 var rhythm_arrow = preload("res://entities/heart/rhythm_arrow.tscn")
+var successful_arrow : Node = null
 var all_arrows : Array[CharacterBody2D] = []
 var active_arrows : Array[CharacterBody2D] = []
 var despawn_indices : Array[int] = []
-var is_active: bool = false
+var is_active: bool = true
 ### Variables for sprite directions ###
 var sprite_left : int = 3
 var sprite_up : int = 0
@@ -29,19 +34,21 @@ func _ready() -> void:
 	right_spawn = arrow_spawns[3]
 
 func _physics_process(delta: float) -> void:
-	var arrow_index = 0
-	for arrow in all_arrows:
-		arrow.velocity.y = arrow_speed
-		arrow.move_and_slide()
-		
-		# Add active arrows to the active arrow array
-		if arrow.position.y <= arrow_despawn_point.position.y:
-			active_arrows.append(arrow)
-		else:
-			#Track the indices of arrows to despawn
-			despawn_indices.append(arrow_index)
-		
-		arrow_index += 1
+	move_arrows()
+	
+	if is_active:
+		if Input.is_action_just_pressed("move_right"):
+			if right_hitbox.has_overlapping_bodies():
+				successful_arrow = right_hitbox.get_overlapping_bodies()[0]
+		if Input.is_action_pressed("move_left"):
+			if left_hitbox.has_overlapping_bodies():
+				successful_arrow = left_hitbox.get_overlapping_bodies()[0]
+		if Input.is_action_pressed("move_down"):
+			if down_hitbox.has_overlapping_bodies():
+				successful_arrow = down_hitbox.get_overlapping_bodies()[0]
+		if Input.is_action_pressed("move_up"):
+			if up_hitbox.has_overlapping_bodies():
+				successful_arrow = up_hitbox.get_overlapping_bodies()[0]
 	
 	#Free despawning arrows from the queue
 	for i in despawn_indices:
@@ -51,6 +58,24 @@ func _physics_process(delta: float) -> void:
 	#Swap arrow array with active arrow array, then empty active arrow array
 	all_arrows = active_arrows
 	active_arrows = []
+
+func move_arrows() -> void:
+	var arrow_index = 0
+	for arrow in all_arrows:
+		arrow.velocity.y = arrow_speed
+		arrow.move_and_slide()
+		
+		if arrow == successful_arrow:
+			#Mark succesful arrows to be removed
+			despawn_indices.append(arrow_index)
+		elif arrow.position.y <= arrow_despawn_point.position.y:
+			# Add active arrows to the active arrow array
+			active_arrows.append(arrow)
+		else:
+			#Track the indices of arrows to despawn
+			despawn_indices.append(arrow_index)
+		
+		arrow_index += 1
 
 func _on_heart_rythm_spawn_arrow() -> void:
 	#Create a new rhythm arrow, add it to the array of active arrows
