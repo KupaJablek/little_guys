a6fd1a1d3a7ebf5ea73ce15141c17748664531e2
YertleT <Ian.g128@hotmail.ca>
2025-04-07 22:14:40 -0400

    Added music node
    Added node for background music and set up music functionality for switching between games

--- levels/platformer/platformer.gd
+++ levels/platformer/platformer.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var player : CharacterBody2D = $platformer_player
 @onready var deoxygenated_cell : Area2D = $deoxygenated_cell
 @onready var oxygen_depot : Area2D = $oxygen_depot
+@onready var bg_music : AudioStreamPlayer = $background_music
 
 @export var oxygen_needed : int = 10
 
@@ -10,6 +11,9 @@ var is_active : bool = true
 var cells_oxygenated : int = 0
 
 signal task_complete
+
+func _ready() -> void:
+	bg_music.play()
 
 func _physics_process(delta: float) -> void:
 	if is_active:
@@ -29,3 +33,8 @@ func _physics_process(delta: float) -> void:
 func set_active_state(active_state : bool) -> void:
 	is_active = active_state
 	player.can_move = active_state
+	
+	if active_state:
+		bg_music.volume_db = 0.0
+	else:
+		bg_music.volume_db = -80.0
