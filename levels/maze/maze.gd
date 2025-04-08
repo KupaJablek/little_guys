cfb7f8f91b91b824002503bc0eca6d5cc6a26e57
Robert Turnbull <rturnbull08@gmail.com>
2025-04-08 01:04:46 -0400

    Finalized majority of the maze game

--- levels/maze/maze.gd
+++ levels/maze/maze.gd
@@ -10,21 +10,22 @@ class_name Maze
 @onready var background: TileMapLayer = $Background
 @onready var checkpoints: TileMapLayer = $Checkpoints
 @onready var completion: Area2D = $Completion
-@onready var player = null
+@onready var player: MazePlayer = $Player
 
 var start: Vector2i
 var end: Vector2i
 var visited: Array[Vector2i] = []
-var grid: Array[Vector2i] = [] #Grid stating if we have walls or not
+var paths: Array[Vector2i] = [] #List of path positions
+var is_active: bool = false
 
 
 func _ready():
-	#Set background.  This will not change
 	generate_maze()
 
 
 func generate_maze() -> void:
-	grid.clear()
+	paths.clear()
+	checkpoints.clear()
 	clear()
 	
 	for x in size.x:
@@ -33,7 +34,7 @@ func generate_maze() -> void:
 	
 	for x in float(size.x + 1) / 2:
 		for y in float(size.y + 1) / 2:
-			grid.append(Vector2i(2 * int(x), 2 * int(y)))
+			paths.append(Vector2i(2 * int(x), 2 * int(y)))
 	
 	var current_cell: Vector2i = Vector2i.ZERO
 	visited = [current_cell]
@@ -48,11 +49,12 @@ func generate_maze() -> void:
 			var random_neighbour = neighbours[randi() % len(neighbours)]
 			stack.push_front(current_cell)
 			var wall: Vector2i = (random_neighbour - current_cell) / 2 + current_cell
-			grid.append(wall)
+			paths.append(wall)
 			current_cell = random_neighbour
 			visited.append(current_cell)
 		elif len(stack) > 0:
 			current_cell = stack.pop_front()
+	
 	
 	var random_start: int = randi_range(0, size.x + size.y)
 	if random_start <= size.x:
@@ -61,17 +63,24 @@ func generate_maze() -> void:
 	else:
 		start = Vector2i(0, random_start - size.x)
 		end = Vector2i(size.x - 1, size.y - (random_start - size.x))
+	if start not in paths:
+		start = paths[0]
 	
-	set_cells_terrain_connect(grid, 0, 0)
+	set_cells_terrain_connect(paths, 0, 0)
 	for x in size.x:
 		for y in size.y:
-			if Vector2i(x, y) not in grid:
+			if Vector2i(x, y) not in paths:
 				set_cell(Vector2i(x, y))
 	
 	checkpoints.set_cell(end, 0, completion_tile_location)
-	#Set the players starting location to start
+	player.position = Vector2(tile_set.tile_size * start) + tile_set.tile_size * 0.5
 	completion.position = Vector2(tile_set.tile_size * end) + tile_set.tile_size * 0.5
 
 
 func _on_completion_entered(body: Node2D) -> void:
-	pass #This is our win condition.  We need to add extra dopamine?  Clear our objective and regenerate the maze. Play win sound?
+	if not body is MazePlayer:
+		return
+	generate_maze()
+	#Play win sound
+	#Clear objective action
+	#Add dopamine
